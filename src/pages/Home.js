4ae47ec3745820fe7e39a6875d28f4eae1fb6e74
Ivan53135 <ikozlov15.ik@gmail.com>
2021-03-28 23:16:49 +0300

    commit message

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+//Page Components
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+import WorksSection from "../components/WorksSection"
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+import ScrollTop from "../components/ScrollTop";
+
+const Home = () => {
+  return (
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
+      <AboutSection />
+      <ServicesSection />
+      <WorksSection />
+      <FaqSection />
+      <ScrollTop />
+    </motion.div>
+  );
+};
+
+export default Home;
